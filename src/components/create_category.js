923913da5e644c53253c9303b8842442d04ba639
James Kariuki <karyorkir@gmail.com>
2018-02-26 18:50:02 +0300

    [Feature #155511944] connect react category registration form to the api endpoint 	src/App.js - route homepage 	src/components/create_category.js - creates the category. 	src/components/home.js - displays the created category. 	src/components/categories.js - renames the file.

---
+++ src/components/create_category.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+class Create_category extends Component {
+    constructor(props){
+        super(props);
+        this.state = { category_name: ""}
+    }
+
+    inputHandler = (event) => {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    };
+
+    create_category_handler = (event) => {
+        const {category_name} = this.state;
+        event.preventDefault();
+        const header = {headers:{'x-access-token': window.localStorage.getItem('token')}, content_type: 'application/json'};
+        axios.post("http://127.0.0.1:5000/categories/", {category_name}, header)
+            .then(response => {
+                this.setState({mess: response.data.message, error: "",});
+                console.log(response.data)
+            })
+            .catch(error => {
+                if (error.response) {
+                    this.setState({error: error.response.data.message, mess: ""});
+                    console.log(error.response.data.message)
+                } else if (error.request) {
+                    console.log("ERRROR")
+                }
+            });
+    };
+    render() {
+        return (
+            <div className="modal fade" id="createcategory" tabindex = "-1" role="dialog"
+                 aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div class="modal-header">
+                            <h5 className="modal-title" id="exampleModalLongTitle">Create category</h5>
+                        </div>
+                        <div className="modal-body">
+                            {this.state.mess?
+                            <div className="alert alert-success">{this.state.mess}</div>: ""}
+                            {this.state.error?
+                                <div className="alert alert-danger">{this.state.error}</div>: ""}
+                            <form className="form-horizontal" onSubmit={this.create_category_handler}>
+                                <div className="form-group">
+                                    <label className="control-label col-sm-3">Category Name:</label>
+                                    <div className="col-sm-9">
+                                        <input type="text" name="category_name" className="form-control" placeholder="Enter Category name"
+                                               value={this.state.category_name} onChange={this.inputHandler}/>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                                    <button type="submit" className="btn btn-primary">
+                                        <i class="glyphicon glyphicon-plus"></i> Add Category
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Create_category;
--- src/components/home.js
+++ src/components/home.js
@@ -1,40 +1,71 @@
 import React, { Component } from 'react';
-import Toaster from './toasterSuccess'
-import {panel_details, category_buttons } from '../App.css'
-import Header from './navbar'
+import Toaster from './toasterSuccess';
+import {panel_details, category_buttons } from '../App.css';
+import Header from './navbar';
+import CreateCategory from './create_category';
+import axios from 'axios';
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {categories: []};
+    }
+    view_categories_handler = () => {
+        const header = {headers:{'x-access-token': window.localStorage.getItem('token')},
+            content_type: 'application/json'};
+        axios.get("http://127.0.0.1:5000/categories/",  header)
+            .then(response => {
+                this.setState({categories:response.data.categories});
+            })
+            .catch(error => {
+                if (error.response) {
+                    this.setState({error: error.response.data.message});
+                } else if (error.request) {
+                    document.getElementById("post_info").innerHTML = "Network error!";
+                }
+            });
+    };
+    componentDidMount(){
+        this.view_categories_handler();
+    }
+
     render() {
+        const categories = this.state.categories;
         return (
             <div>
                 <Header/>
                 <Toaster/>
+                <CreateCategory/>
             <div className="container">
-                <div className="panel_details">
-                    <div className="col-sm-14">
-                        <div className="panel panel-info">
+                    <div className="col-sm-offset-1 col-sm-14">
+                        <div className="panel panel-primary">
                             <div className="panel-heading">
-
-                                <div className="col-sm-9"><span className="glyphicon glyphicon-list"></span> Category List</div>
-                                    <div className="col-sm-offset-2">
-                                        <form className="navbar-form" role="search">
-                                            <div className="input-group add-on col-sm-12">
-                                                <input type="text" className="form-control" placeholder="Search for a category..." name="srch-term"/>
-                                                    <div className="input-group-btn">
-                                                        <button className="btn btn-default" type="submit">
-                                                            <i class="glyphicon glyphicon-search"></i></button>
-                                                    </div>
-                                            </div>
-                                        </form>
+                                     <form className="navbar-form" role="search">
+                                         <div className="col-sm-2">
+                                            <i className="glyphicon glyphicon-list"></i> Category List
+                                         </div>
+                                    <div className="input-group add-on col-sm-9">
+                                        <input type="text" className="form-control" placeholder="Search
+                                                for a category..." name="srch-term"/>
+                                        <div className="input-group-btn">
+                                            <button className="btn btn-default" type="submit">
+                                                <i class="glyphicon glyphicon-search"></i></button>
+                                        </div>
                                     </div>
+                                </form>
                             </div>
                             <div className="panel-body">
 
+                                    <a href="#createcategory" data-toggle="modal" className="btn btn-primary"
+                                   data-target="#createcategory"><i class="glyphicon glyphicon-plus"></i>
+                                        Create Category</a>
+                            <hr/>
+
+                                { categories.map((categories) => (
                                 <div className="list-group">
                                     <span className="list-group-item">
                                         <span className="checkbox">
-                                            <input type="checkbox" id="checkbox" />
-                                            <label for="checkbox">
-                                                List group item heading 1
+                                            <label>
+                                                {categories.category_name}
                                             </label>
                                         <span className="category_buttons action-buttons">
                                             <a href="#"><span class="glyphicon glyphicon-trash"></span></a>
@@ -45,53 +76,8 @@ class Home extends Component {
                                     </span>
                                 </div>
 
-                                <div className="list-group">
-                                    <span className="list-group-item">
-                                        <span className="checkbox">
-                                            <input type="checkbox" id="checkbox" />
-                                            <label for="checkbox">
-                                                List group item heading
-                                            </label>
-                                        <span className="category_buttons action-buttons">
-                                            <a href="#" className="trash"><span class="glyphicon glyphicon-trash"></span></a>
-                                            <a href="#"><span className="glyphicon glyphicon-pencil"></span></a>
-                                            <a href="#" className="flag"><span class="glyphicon glyphicon-eye-open"></span></a>
-                                        </span>
-                                    </span>
-                                    </span>
-                                </div>
+                                ))}
 
-                                <div className="list-group">
-                                    <span className="list-group-item">
-                                        <span className="checkbox">
-                                            <input type="checkbox" id="checkbox" />
-                                            <label for="checkbox">
-                                                List group item heading
-                                            </label>
-                                        <span className="category_buttons action-buttons">
-                                            <a href="#" className="trash"><span class="glyphicon glyphicon-trash"></span></a>
-                                            <a href="#"><span className="glyphicon glyphicon-pencil"></span></a>
-                                            <a href="#" className="flag"><span class="glyphicon glyphicon-eye-open"></span></a>
-                                        </span>
-                                    </span>
-                                    </span>
-                                </div>
-
-                                <div className="list-group">
-                                    <span className="list-group-item">
-                                        <span className="checkbox">
-                                            <input type="checkbox" id="checkbox" />
-                                            <label for="checkbox">
-                                                List group item heading
-                                            </label>
-                                        <span className="category_buttons action-buttons">
-                                            <a href="#" className="trash"><span class="glyphicon glyphicon-trash"></span></a>
-                                            <a href="#"><span className="glyphicon glyphicon-pencil"></span></a>
-                                            <a href="#" className="flag"><span class="glyphicon glyphicon-eye-open"></span></a>
-                                        </span>
-                                    </span>
-                                    </span>
-                                </div>
 
                             </div>
                             <div className="panel-footer">
@@ -103,7 +89,8 @@ class Home extends Component {
                                     <div className="col-md-6">
                                         <ul className="pagination pagination-sm pull-right">
                                             <li className="disabled"><a href="javascript:void(0)">«</a></li>
-                                            <li className="active"><a href="javascript:void(0)">1 <span className="sr-only">(current)</span></a></li>
+                                            <li className="active"><a href="javascript:void(0)">1
+                                                <span className="sr-only">(current)</span></a></li>
                                             <li><a href="#">2</a></li>
                                             <li><a href="#">3</a></li>
                                             <li><a href="#">4</a></li>
@@ -116,7 +103,6 @@ class Home extends Component {
                         </div>
                     </div>
                 </div>
-            </div>
             </div>
         );
     }

5c609c18a886845c4b1cba6ad425f8064e17b6c8
James Kariuki <karyorkir@gmail.com>
2018-03-02 12:25:06 +0300

    [Feature #155637148] Connect API logout endpoint to the react UI
    	src/App.js- add a link for logout
    	src/components/logout.js - add logout functionality and connection.
    	src/components/navbar.js - link logout button with the functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {Route,BrowserRouter as Router, Switch, Link } from 'react-router-dom'
-import Info from "./components/jumbotron"
-import Login from "./components/login"
-import Register from "./components/register"
-import Categories from "./components/create_category"
+import {Route,BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import Info from "./components/jumbotron";
+import Login from "./components/login";
+import Logout from "./components/logout"
+import Register from "./components/register";
+import Categories from "./components/create_category";
 import ResetPassword from "./components/reset_password";
 import Home from "./components/home";
 import './App.css';
@@ -16,6 +17,7 @@ class App extends Component {
               <Switch>
                   <Route exact path="/" component={Info} />
                   <Route exact path="/login" component={Login} />
+                  <Route exact path="/logout" component={Logout} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/categories" component={Categories} />
                   <Route exact path="/reset_password" component={ResetPassword} />
